7e0b84ae36ae3344ae34bca63b8a750cad56117c
Aiden Lee <aiden.lee.jh@gmail.com>
2017-11-17 14:50:53 -0800

    add events on service worker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,11 @@
-console.log('service worker running!');
+self.addEventListener('install', function() {
+	console.log('Install!');
+});
+
+self.addEventListener('activate', function() {
+	console.log('Activate!');
+});
+
+self.addEventListener('fetch', function(event) {
+	console.log('Fetch!', event.request);
+});
