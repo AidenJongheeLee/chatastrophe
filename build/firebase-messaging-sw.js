38fc271f2a127979b12fa14daa41595825346ecc
Aiden Lee <aiden.lee.jh@gmail.com>
2017-11-19 17:51:07 -0800

    added notification function

---
+++ build/firebase-messaging-sw.js
@@ -0,0 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/4.6.2/firebase.js');
+importScripts('https://www.gstatic.com/firebasejs/4.6.2/firebase-messaging.js');
+
+firebase.initializeApp({
+	messagingSenderId: '676276786469'
+});
